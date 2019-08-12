08c321727b0615a9fd5c47dc2ab5e50be91edc60
Sahaj <gpt.sahaj28@gmail.com>
2019-08-12 19:33:25 +0530

    first commit

---
+++ Rename.py
@@ -0,0 +1,15 @@
+import os
+
+os.chdir('path')
+for fiel in os.listdir():
+	file_name, file_ext = os.path.splitext(fiel)
+	try:
+		file_title, file_num = file_name.split(' ')
+	except Exception:
+		file_name+=" (0)" 
+		file_title, file_num = file_name.split(' ')
+	file_num = str(int(file_num.strip()[1:-1])+1).zfill(2)
+	file_name = 'file name {}{}'.format(file_num, file_ext)	
+	os.rename(fiel, file_name)
+	print(file_name)
+
